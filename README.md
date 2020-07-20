73c41ef050e67f0e37c821a6a44c7bb2013c6ca0
Oh Han Yi <ohy1994@gmail.com>
2020-07-20 21:44:34 +0800

    Add 403 handling and instructions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # stockx-inventory-updater
 
-![Screenshot](https://i.imgur.com/RIFEo54.png)
+![Screenshot](https://i.imgur.com/FsZhTF3.png)
 
 ## Installation
 ```
@@ -18,12 +18,15 @@ Currency and row to start with can be edited in the **`config.json`** file.
 ## How to use
 1. Fill in style code and size in the workbook provided.
 2. Open terminal or command prompt and cd to the directory.
-2. python3 **`stockx-inventory-updater.py`**
+3. python3 **`stockx-inventory-updater.py`**
 
 ## Bugs
-If you encounter captcha or 403 errors, try solving captcha by browsing the StockX website manually and see if it disappears.
+403 errors are caused by too many requests from the same IP in a short time. \
+Once encountered, click [Here](https://stockx.com/api/browse?) on an incognito browser and solve the captcha. \
+The workaround now would be to change the starting row if the script encounters a 403 to continue from there. \
 Feel free to report any bugs running the script.
 
 ## To-do
-* Autorun script daily or weekly
+* Database to store all sales of a fetched product
+* Autorun script daily or weekly (tough with dem captchas)
 * Notify user if desired price is far off the average sale or last sale
--- stockx-inventory-updater.py
+++ stockx-inventory-updater.py
@@ -4,6 +4,7 @@ from openpyxl import load_workbook
 from forex_python.converter import CurrencyRates
 from datetime import datetime
 import json
+import time
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -31,53 +32,72 @@ def main():
     wb = load_workbook(resource_path("./stockx_book.xlsx"))
     ws = wb['Sheet1']
 
+    session = requests.Session()
+    session.headers.update({
+        'content-type': 'application/x-www-form-urlencoded',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+        'accept': '*/*',
+        'accept-ending': 'gzip, deflate, br',
+        'connection': 'keep-alive'
+    })
+    # session.get("https://stockx.com/api/browse?&_search=EF2367&dataType=product")
+    # print(session.cookies)
+
+    row_num = config["start-row"]
+
     for row in ws.iter_rows(min_row=config["start-row"]): 
         if row[0].value == None:
             break
-        print(f'Doing {row[0].value}')
-        urlkey = search_product(row[0].value)
+        print(f'Fetching row {row_num}: {row[0].value}')
+        urlkey = search_product(row[0].value, session)
+        if urlkey is None:
+            break
         row[6].value = "Stockx"
         row[6].hyperlink = f'https://stockx.com/{urlkey}'
-        result = product_info(urlkey, str(row[2].value))
+        result = product_info(urlkey, str(row[2].value), session)
+        if result is None:
+            break
         row[1].value = result["title"]
-        if result["uuid"] == None:
+        if result["uuid"] is None:
             print("Size not found, check if W or Y is needed.")
-        sales = get_sales(result["uuid"])
+            break
+        sales = get_sales(result["uuid"], session)
         row[4].value = round((sales["last"] * rate), 2)
         row[5].value = round((sales["average"] * rate), 2)
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %I:%M:%S %p")
         row[7].value = dt_string
+        row_num += 1
         
-    wb.save(resource_path("./stockx_book.xlsx"))
+    wb.save(resource_path("./stockx_book_output.xlsx"))
 
-def search_product(sku):
+def search_product(sku, session):
     url = f'https://stockx.com/api/browse?&_search={sku}&dataType=product'
-    headers = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        'accept': '*/*',
-        'accept-ending': 'gzip, deflate, br',
-        'connection': 'keep-alive'
-    }
-    req = requests.get(url, headers=headers)
+    # headers = {
+    #     'content-type': 'application/x-www-form-urlencoded',
+    #     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+    #     'accept': '*/*',
+    #     'accept-ending': 'gzip, deflate, br',
+    #     'connection': 'keep-alive'
+    # }
+    req = session.get(url)
     if req.status_code == 200:
         data = req.json()
         urlkey = data["Products"][0]["urlKey"]
         return urlkey
     else:
-        print(f'Error {req.status_code}')
+        print(f'Error {req.status_code} search_product')
     
-def product_info(urlkey, size):
+def product_info(urlkey, size, session):
     url = f'https://stockx.com/api/products/{urlkey}?includes=market&currency=USD'
-    headers = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        'accept': '*/*',
-        'accept-ending': 'gzip, deflate, br',
-        'connection': 'keep-alive'
-    }
-    req = requests.get(url, headers=headers)
+    # headers = {
+    #     'content-type': 'application/x-www-form-urlencoded',
+    #     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+    #     'accept': '*/*',
+    #     'accept-ending': 'gzip, deflate, br',
+    #     'connection': 'keep-alive'
+    # }
+    req = session.get(url)
     if req.status_code == 200:
         data = req.json()
         result = {}
@@ -90,18 +110,18 @@ def product_info(urlkey, size):
                 break
         return result
     else:
-        print(f'Error {req.status_code}')
+        print(f'Error {req.status_code} product_info')
 
-def get_sales(uuid):
+def get_sales(uuid, session):
     url = f'https://stockx.com/api/products/{uuid}/activity?state=480&currency=USD&limit=3&page=1&sort=createdAt&order=DESC'
-    headers = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        'accept': '*/*',
-        'accept-ending': 'gzip, deflate, br',
-        'connection': 'keep-alive'
-    }
-    req = requests.get(url, headers=headers)
+    # headers = {
+    #     'content-type': 'application/x-www-form-urlencoded',
+    #     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+    #     'accept': '*/*',
+    #     'accept-ending': 'gzip, deflate, br',
+    #     'connection': 'keep-alive'
+    # }
+    req = session.get(url)
     if req.status_code == 200:
         data = req.json()
         amount = 0
@@ -116,6 +136,7 @@ def get_sales(uuid):
         sales["average"] = round(average, 2)
         return sales
     else:
-        print(f'Error {req.status_code}')
+        print(f'Error {req.status_code} get_sales')
     
-main()
+if __name__ == '__main__':
+    main()
